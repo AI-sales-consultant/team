96d459c92e495cca4e41be5b498eea73e3705c9b
Ma <dasklagelied@163.com>
2025-07-28 16:17:35 +0100

    reslove frontend dependency issue

--- frontend/postcss.config.mjs
+++ frontend/postcss.config.mjs
@@ -1,8 +1,9 @@
 /** @type {import('postcss-load-config').Config} */
 const config = {
   plugins: {
-    tailwindcss: {},
+    '@tailwindcss/postcss': {}, // <--- 修改此行
+    autoprefixer: {},
   },
-};
+}
 
 export default config;
