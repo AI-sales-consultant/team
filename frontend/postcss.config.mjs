bbfc441f40eab335cfc7d473704d3ca81ff576a3
ZhexingYoung <ec24179@bristol.ac.uk>
2025-07-28 22:43:10 +0100

    Revert "reslove frontend dependency issue" 修复css无法加载
    This reverts commit 96d459c92e495cca4e41be5b498eea73e3705c9b.

--- frontend/postcss.config.mjs
+++ frontend/postcss.config.mjs
@@ -1,9 +1,8 @@
 /** @type {import('postcss-load-config').Config} */
 const config = {
   plugins: {
-    '@tailwindcss/postcss': {}, // <--- 修改此行
-    autoprefixer: {},
+    tailwindcss: {},
   },
-}
+};
 
 export default config;
