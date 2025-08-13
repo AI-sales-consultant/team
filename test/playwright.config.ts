8ed7a3fca3f3efec9dd6aae3bcdbb10658780b6b
KeithCai1995 <um24581@bristol.ac.uk>
2025-08-13 19:50:34 +0800

    Update playwright.config.ts

--- test/playwright.config.ts
+++ test/playwright.config.ts
@@ -1,7 +1,13 @@
-
 import { defineConfig } from '@playwright/test'
+
 export default defineConfig({
   testDir: './test/frontend/e2e',
-  use: { baseURL: 'http://localhost:3000' },
+  use: {
+    baseURL: 'http://localhost:3000',
+    trace: 'on-first-retry',
+    screenshot: 'only-on-failure',
+    video: 'retain-on-failure'
+  },
+  retries: 1,
   reporter: [['list'], ['html', { outputFolder: 'test/playwright-report', open: 'never' }]]
 })
